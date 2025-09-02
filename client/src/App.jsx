51601be7bde830175f3897c251645738d6867fda
Vishal Nagar <102690538+vishal-nagar744@users.noreply.github.com>
2025-09-02 18:21:16 +0530

    Initialize React + Vite client app
    Add initial project files for a React application using Vite, including configuration for ESLint, Tailwind CSS, and basic setup files. This sets up the project structure and dependencies for development.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center p-4 bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
